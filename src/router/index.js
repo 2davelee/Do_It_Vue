fb83633a219a3cb5be137cb6ae7a96c3f0912436
2davelee <pboys11@gmail.com>
2025-02-03 22:35:33 +0900

    these are for deploy3

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import { createRouter, createWebHistory } from "vue-router"; // createRouter, createWebHistory import 추가
 import pageRouter from "./modules/pageRouter";
 
-Vue.use(VueRouter);
+Vue.use(VueRouter); // Vue Router 사용 선언 (필수)
 
-const router = createRouter({
-  history: createWebHistory("/Do_It_Vue/"), // history 모드 설정
-  routes: [pageRouter], // routes 설정
+const router = new VueRouter({ // VueRouter 인스턴스 생성 (Vue 2 방식)
+  mode: 'history', // 또는 'hash' 모드 (Vue 2 방식)
+  base: '/Do_It_Vue/', // base URL 설정 (원하는 경우)
+  routes: pageRouter, // routes 설정
 });
 
 export default router;
